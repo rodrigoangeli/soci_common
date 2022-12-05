1303acf8f5b9f0af6e62298f706b2faf86668d12
Rodrigo Angeli <rodrigoangeli@yahoo.com>
2022-12-04 22:14:33 -0300

    Updates

---
+++ src/middlewares/require-auth.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { NotAuthorizedError } from "../../../common/src/errors/not-authorized-error";
+
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.currentUser) {
+    throw new NotAuthorizedError();
+  }
+  next();
+};
