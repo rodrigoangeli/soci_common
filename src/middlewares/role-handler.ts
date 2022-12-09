2fec9514728a8fa0d6908cfce1b56bf0a4b21cdd
Rodrigo Angeli <rodrigoangeli@yahoo.com>
2022-12-09 05:56:42 -0300

    Updates

---
+++ src/middlewares/role-handler.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { NotAuthorizedError } from "../errors/not-authorized-error";
+import { BadRequestError } from "../errors/bad-request-error";
+import { AccessControl } from "accesscontrol";
+
+type ActionType = "createAny" | "updateAny" | "deleteAny" | "readAny";
+
+export const requireRole = (
+  permissions: any,
+  action: ActionType,
+  resource: any
+) => {
+  const ac = new AccessControl(permissions);
+  return (req: Request, res: Response, next: NextFunction) => {
+    const user = req.currentUser!;
+    const permission = ac.can(user.role)[action](resource);
+    if (Object.keys(permission).length === 0) {
+      throw new BadRequestError("No permission was provided");
+    }
+    if (permission.granted) next();
+    else throw new NotAuthorizedError();
+  };
+};
