74d561ad387b84e06291857ae41f2205c6ead297
Rodrigo Angeli <rodrigoangeli@yahoo.com>
2024-01-30 09:51:54 -0300

    Updates

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -1,13 +1,34 @@
 import { NextFunction, Request, Response } from "express";
 import { NotAuthorizedError } from "../errors/not-authorized-error";
+import jwt from "jsonwebtoken";
+
+interface UserPayload {
+  id: string;
+  email: string;
+  role: string;
+  company_id: number;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: UserPayload;
+    }
+  }
+}
 
 export const requireAuth = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (!req.currentUser) {
-    throw new NotAuthorizedError();
-  }
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer TOKEN
+
+  if (token == null) return new NotAuthorizedError(); // No token provided
+
+  const payload = jwt.verify(token, process.env.JWT_KEY!) as UserPayload;
+
+  req.user = payload;
   next();
 };
