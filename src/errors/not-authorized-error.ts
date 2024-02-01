4bd238483f17493e38881c9c8277521280add8f4
Rodrigo Angeli <rodrigoangeli@yahoo.com>
2024-01-31 23:10:29 -0300

    Updates

--- src/errors/not-authorized-error.ts
+++ src/errors/not-authorized-error.ts
@@ -2,11 +2,11 @@ import { CustomError } from "./custom-error";
 
 export class NotAuthorizedError extends CustomError {
   statusCode = 401;
-  constructor() {
-    super("Not authorized");
+  constructor(public message: string = "Not authorized") {
+    super(message);
     Object.setPrototypeOf(this, NotAuthorizedError.prototype);
   }
   serializeErrors() {
-    return [{ message: "Not authorized" }];
+    return [{ message: this.message }];
   }
 }
--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -24,18 +24,13 @@ export const requireAuth = (
 ) => {
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(" ")[1]; // Bearer TOKEN
-  console.log("authHeader", authHeader);
   if (!token || token === null) {
-    throw new NotAuthorizedError();
+    return next(new NotAuthorizedError("No token provided"));
   }
-  console.log("token", token);
-
   jwt.verify(token, process.env.JWT_KEY!, (err, user) => {
     if (err) {
-      console.log("err", err);
-      throw new NotAuthorizedError();
+      return next(new NotAuthorizedError("jwt"));
     } else {
-      console.log("else", user);
       req.user = user as UserPayload;
       next();
     }
