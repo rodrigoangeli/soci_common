1a99d62b546e82d812569f0b71513e08ba6d86d3
Rodrigo Angeli <rodrigoangeli@yahoo.com>
2024-06-16 21:55:44 -0300

    Updates

--- src/events/services/crm/activity-event.ts
+++ src/events/services/crm/activity-event.ts
@@ -3,6 +3,8 @@ import { Subjects } from "../../subjects";
 export interface ActivityEvent {
   subject: Subjects.Activity;
   data: {
+    company_id: number;
+    lead_id: number;
     type: string;
     details: any;
     timestamp: any;
