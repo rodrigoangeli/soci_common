d923876ffd1c4b896f3b04f24f035b7a610eb799
Rodrigo Angeli <rodrigoangeli@yahoo.com>
2024-07-02 02:08:46 -0300

    Updates

--- src/events/services/payments/subscription-created-event.ts
+++ src/events/services/payments/subscription-created-event.ts
@@ -4,5 +4,11 @@ export interface SubscriptionCreatedEvent {
   subject: Subjects.SubscriptionCreated;
   data: {
     company_id: number;
+    stripeCustomerId: string;
+    stripeSubscriptionId: string;
+    plan: string;
+    status: string;
+    currentPeriodEnd: Date;
+    isTrial: boolean;
   };
 }
