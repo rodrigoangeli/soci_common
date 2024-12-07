1c401dc431be5ecb40d2254812ddb00a7bd76a12
Rodrigo Angeli <rodrigoangeli@yahoo.com>
2024-12-07 16:34:59 -0300

    Updates

--- src/events/services/emails/fetch-segmentation-event.ts
+++ src/events/services/emails/fetch-segmentation-event.ts
@@ -3,6 +3,7 @@ import { Subjects } from "../../subjects";
 export interface FetchSegmentationEvent {
   subject: Subjects.FetchSegmentationEvent;
   data: {
+    company_id: number;
     to: string;
     replyTo: string;
   };
